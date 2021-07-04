700542cd0b57a3c849bea3ec8d79b828e1095269
amine <amine@alphorm.com>
2021-07-04 23:55:21 +0100

    i'm out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg'
 import './App.css'
+import memo from './memo.png'
 
 function App() {
   return (
@@ -7,20 +7,22 @@ function App() {
       <div className='header'>
         <div className='container'>
           <div className='navbarContainer'>
-            <img src='https://i.pinimg.com/originals/61/af/37/61af370544463d4b8db051d1149f830d.png' alt='logo' className='logo' />
+            <img src='https://raw.githubusercontent.com/medaminehamidi/sunset/main/src/assets/logo.png' alt='logo' className='logo' />
           </div>
           <div className='header-text-container'>
             <h1 className='header-title'>Made with love</h1>
             <p className='header-subtitle'>Making delicuous food since 1002</p>
-            <p className='header-subtitle'>Making delicuous food since 1002</p>
+            <p className='header-subtitle'>Booking online or call +2120665656565 </p>
             <button className='header-button'>Reserve your place</button>
           </div>
           <div className='paper-container'>
             <div className='left-container'>
-              <img src='https://d25tv1xepz39hi.cloudfront.net/2020-05-27/files/15905755410.jpg' alt='' className='paper-image-1' />
-              <img src='https://d25tv1xepz39hi.cloudfront.net/2020-05-27/files/15905755410.jpg' alt='' className='paper-image-1' />
+              <img src='https://scontent.frba3-2.fna.fbcdn.net/v/t1.6435-9/105685031_173088807525119_8247975899114727059_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=a26aad&_nc_eui2=AeGo6fxEWIYP2T-am-8tsSlLsQpMaQxKnGixCkxpDEqcaJgBae0JgO92jNIC3RyCuXlpm0rbtJuL6cNY2tIdTxG8&_nc_ohc=ThROLyb0piwAX_E5hxf&_nc_ht=scontent.frba3-2.fna&oh=03283d230a1120cf66c09505858630de&oe=60E6E962' alt='' className='paper-image-1' />
+              <img src='https://scontent.frba3-2.fna.fbcdn.net/v/t1.6435-9/105685031_173088807525119_8247975899114727059_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=a26aad&_nc_eui2=AeGo6fxEWIYP2T-am-8tsSlLsQpMaQxKnGixCkxpDEqcaJgBae0JgO92jNIC3RyCuXlpm0rbtJuL6cNY2tIdTxG8&_nc_ohc=ThROLyb0piwAX_E5hxf&_nc_ht=scontent.frba3-2.fna&oh=03283d230a1120cf66c09505858630de&oe=60E6E962' alt='' className='paper-image-1' />
             </div>
             <div className='right-container'>
+              <h1 className='right-section-title'>Our Story</h1>
+              <div className='divider right-section' />
               <p className='paper-text'>Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002</p>
             </div>
           </div>
@@ -30,46 +32,68 @@ function App() {
         <div className='container'>
           <p className='body-text-1'>Making delicuous food since 1002Making delicuous food since elicuous food since 1002Making delicuous food since elicuous food since 1002Making delicuous food since elicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002</p>
         </div>
+        <div className='container'>
+          <div className='lables'>
+            <div className='lables-subs'>
+              <h1 className='right-section-title center'>1564+</h1>
+              <p className='body-text-1 center'>satisfied guests</p>
+            </div>
+            <div className='lables-subs'>
+              <h1 className='right-section-title center'>64+</h1>
+              <p className='body-text-1 center'>drinks to offer</p>
+            </div>
+            <div className='lables-subs'>
+              <h1 className='right-section-title center'>154+</h1>
+              <p className='body-text-1 center'>tables</p>
+            </div>
+            <div className='lables-subs'>
+              <h1 className='right-section-title center'>15+</h1>
+              <p className='body-text-1 center'>team members</p>
+            </div>
+          </div>
+        </div>
         <div className='special-offers-section'>
           <div className='container center-flex'>
-            <h3 className='white-text'>Spacial offers</h3>
+            <h3 className='white-text'>SPECIAL OFFERS</h3>
             <span className='divider' />
             <div className='cards-container'>
               <div className='card'>
                 <img src='https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='' className='card-img' />
                 <div className='card-footer'>
-                  <h4 style={{margin: '8px 0'}}>Title</h4>
-                  <p style={{margin: '0 0 8px 0'}}>Description descritpion</p>
+                  <h4 style={{ margin: '8px 0' }}>Title</h4>
+                  <p style={{ margin: '0 0 8px 0' }}>Description descritpion</p>
                 </div>
               </div>
               <div className='card'>
                 <img src='https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='' className='card-img' />
                 <div className='card-footer'>
-                  <h4 style={{margin: '8px 0'}}>Title</h4>
-                  <p style={{margin: '0 0 8px 0'}}>Description descritpion</p>
+                  <h4 style={{ margin: '8px 0' }}>Title</h4>
+                  <p style={{ margin: '0 0 8px 0' }}>Description descritpion</p>
                 </div>
               </div>
               <div className='card'>
                 <img src='https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='' className='card-img' />
                 <div className='card-footer'>
-                  <h4 style={{margin: '8px 0'}}>Title</h4>
-                  <p style={{margin: '0 0 8px 0'}}>Description descritpion</p>
+                  <h4 style={{ margin: '8px 0' }}>Title</h4>
+                  <p style={{ margin: '0 0 8px 0' }}>Description descritpion</p>
                 </div>
               </div>
               <div className='card'>
                 <img src='https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='' className='card-img' />
                 <div className='card-footer'>
-                  <h4 style={{margin: '8px 0'}}>Title</h4>
-                  <p style={{margin: '0 0 8px 0'}}>Description descritpion</p>
+                  <h4 style={{ margin: '8px 0' }}>Title</h4>
+                  <p style={{ margin: '0 0 8px 0' }}>Description descritpion</p>
                 </div>
               </div>
             </div>
             <div className='paper-container paper-2'>
-              <div className='left-container'>
-                 <p className='paper-text'>Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002</p>
+              <div className='right-container'>
+                <h1 className='right-section-title'>OUR CHIEF</h1>
+                <div className='divider right-section' />
+                <p className='paper-text'>Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002Making delicuous food since 1002</p>
               </div>
               <div className='right-container'>
-                  <img src='https://d25tv1xepz39hi.cloudfront.net/2020-05-27/files/15905755410.jpg' alt='' className='paper-image-2' />
+                <img src={memo} alt='' className='paper-image-2' />
               </div>
             </div>
           </div>
